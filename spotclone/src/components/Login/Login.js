af12f298b51a34009f53ea81c6c15227a9393829
Syndicate555 <saziz028@uottawa.ca>
2020-08-26 03:35:11 -0400

    Update Login.js

--- spotclone/src/components/Login/Login.js
+++ spotclone/src/components/Login/Login.js
@@ -3,11 +3,11 @@ import "./Login.css";
 function Login() {
   return (
     <div className="login">
-      <h1>Login page</h1>
       <img
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
+      <button>LOGIN TO SPOTIFY</button>
     </div>
   );
 }
