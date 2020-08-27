24ec8dc3849a5d37c1a15a704beb3430f65c0b35
Syndicate555 <saziz028@uottawa.ca>
2020-08-27 00:18:17 -0400

    Update spotify.js

--- spotclone/src/spotify.js
+++ spotclone/src/spotify.js
@@ -1,2 +1,5 @@
+// Login button link in the Login.js file which authorize user login login
 export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// The user gets redirected to the following link
 const reditectUri = "http://localhost:3000";
